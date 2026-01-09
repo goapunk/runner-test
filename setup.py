5f93cbbc39f2855e1325cc58bd36438a224466a8
goapunk <julian@dehm.dev>
2026-01-09 14:56:06 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="runner",
+    version="0.1.0",
+    description="Runner Test demo",
+    packages=find_packages(),
+    python_requires=">=3.8",
+)
+
